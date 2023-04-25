50afdae8392b21408a0251735c8e94fe29ce7c74
dasha <dashagrgrv121@gmail.com>
2023-04-25 15:49:00 +0400

    fix form.js file

--- form.js
+++ form.js
@@ -1,69 +1,40 @@
 const btnSubmit = document.querySelector('.btn'),
-    selects = document.getElementsByTagName('select'),
-    comment = document.getElementById('comment'),
-    feedback = document.getElementById('feedback'),
-    div = document.querySelector('.json-container');
+    form = document.getElementById('form'),
+    container = document.querySelector('.json-container'),
+    collection = document.querySelectorAll('.form-control');
 
+function createData(data){
 
-function validation(){
-    let elements = document.getElementById("form").elements;
-    Array.from(elements)
-    .filter((item) => !!item.name)
-    .map((element) => {
-      if(element.value != ''){
-        btnSubmit.disabled = false;
+    const jsonData = [];
+
+    data.forEach((el)=>{
+            const {name, value} = el;
+            jsonData.push({name, value});
         }
-        else{btnSubmit.disabled = true;}
-    });
-}
-
-function DataItem(comment,feedback,value) {
+    );
     
-        this.comment = comment.value;
-        this.feedback = feedback.value;
-        this.selects = value;
-};
-
-
-function createData(){
-
-    let arr = [].slice.call(selects);
-    let value= [];
-
-    arr.forEach(el => {
-        value.push(el.value);
-    });
-    
-    let objData = new DataItem(comment,feedback,value);
-    
-    let jsonData = document.createElement('div');
-    let data = JSON.stringify(objData);
-    
-    jsonData.append(data);
-    div.append(jsonData);
+    container.append(JSON.stringify(jsonData));
     return jsonData;
 }
 
-setInterval(validation, 100);
+async function sendData(){
+    const data = createData(collection);
 
-
-btnSubmit.addEventListener('click', async (e)=>{
-        
-    e.preventDefault();
-    
-    let data = createData();
-    document.getElementById("form").reset();
-
-    
     let res = await fetch('/server/server.js', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: data,
+          body: JSON.stringify(data),
         });
     
     if (res.ok) { 
-        let json = await res.json();
+        let json = res.json();
         alert(json);
     } 
-    else {alert("Ошибка HTTP: " + res.status);}
-});
+    else {
+        alert("Ошибка HTTP: " + res.status);
+    }
+
+    form.reset();
+}
+
+btnSubmit.addEventListener('click',sendData);
